c0f8bd2106660e6ee82d2118b4280a83f1f45e34
riolly <alberto.cahyantara@gmail.com>
2024-08-06 21:12:01 +0900

    turso db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,12 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
+  out: "./drizzle/migrations",
   dialect: "sqlite",
+  driver: "turso",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
   },
   tablesFilter: ["botmind_*"],
 } satisfies Config;
